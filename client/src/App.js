6a4cc6d9d37c6632fdc210e5a099f7a22b9ec673
felipemuner <felipe.muner@gmail.com>
2020-02-13 11:26:23 +0700

    installing react to fetch data from MongoDB with grapQL and show it to the user

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+// import logo from "./logo.svg";
+// import "./App.css";
+
+function App() {
+  return (
+    <div id="">
+      <h1>Felipe Muner Reading list</h1>
+    </div>
+  );
+}
+
+export default App;
