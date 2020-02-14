d415cfdf54206f24017c90fb91d1c6672d09f173
felipemuner <felipe.muner@gmail.com>
2020-02-14 12:17:57 +0700

    set up variable to save a new book into mongoDB

--- client/src/queries/queries.js
+++ client/src/queries/queries.js
@@ -19,8 +19,8 @@ const getAuthorsQuery = gql`
 `;
 
 const addBookMutation = gql`
-  mutation {
-    addBook(name: "", genre: "", authorID: "") {
+  mutation($name: String!, $genre: String!, $authorID: ID!) {
+    addBook(name: $name, genre: $genre, authorID: $authorID) {
       id
       name
     }
