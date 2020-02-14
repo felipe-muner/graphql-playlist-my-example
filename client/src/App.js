5b66f540ff38745faef721acdac344e56b402ebb
felipemuner <felipe.muner@gmail.com>
2020-02-14 09:49:51 +0700

    add book form

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,27 @@
-import React from "react";
-// import logo from "./logo.svg";
-// import "./App.css";
+import React, { Component } from "react";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
 
-function App() {
-  return (
-    <div id="">
-      <h1>Felipe Muner Reading list</h1>
-    </div>
-  );
+import BookList from "./component/BookList";
+import AddBook from "./component/AddBook";
+
+//apollo client setup
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql"
+});
+
+class App extends Component {
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <div id="main">
+          <h1>Ninja's Reading List</h1>
+          <BookList />
+          <AddBook />
+        </div>
+      </ApolloProvider>
+    );
+  }
 }
 
 export default App;
